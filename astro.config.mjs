dffde526893471070c28b29c6337ad59114b66ef
Karl Mantle <130184376+karl-mantle@users.noreply.github.com>
2024-11-11 15:23:53 +0000

    alpine.js

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,10 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
+import alpinejs from '@astrojs/alpinejs';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [sitemap(), tailwind()],
+  integrations: [sitemap(), tailwind(), alpinejs()],
 });
