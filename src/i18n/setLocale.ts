5f9061c897d8afb06c20054453d2c9b82cf8d804
Karl Mantle <130184376+karl-mantle@users.noreply.github.com>
2024-11-12 21:54:02 +0000

    set locale + datetime

---
+++ src/i18n/setLocale.ts
@@ -0,0 +1,30 @@
+import { getLanguage } from '../utils/gqlSettings';
+
+const localeMap = {
+  '': 'en-US',
+  'en_US': 'en-US',
+  'en_GB': 'en-GB',
+  'es_ES': 'es-ES',
+  'fr_FR': 'fr-FR',
+  'de_DE': 'de-DE',
+  'it_IT': 'it-IT',
+  'ja_JP': 'ja-JP',
+  'zh_CN': 'zh-CN',
+  'ru_RU': 'ru-RU',
+  'pt_PT': 'pt-PT',
+  'pt_BR': 'pt-BR',
+  'nl_NL': 'nl-NL',
+  'sv_SE': 'sv-SE',
+  'no_NO': 'no-NO',
+  'da_DK': 'da-DK',
+  'fi': 'fi-FI',
+  'tr_TR': 'tr-TR',
+  'ko_KR': 'ko-KR',
+  // to do add more of these...
+};
+
+export async function setLocale() {
+  const res = await getLanguage();
+  const { language } = res;
+  return localeMap[language] || 'en-US';
+}
