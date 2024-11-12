47595da5b37b99a8bd2bb91d33e4751f5caf14d4
Karl Mantle <130184376+karl-mantle@users.noreply.github.com>
2024-11-12 21:54:45 +0000

    basic auth copy

--- src/utils/wpQuery.ts
+++ src/utils/wpQuery.ts
@@ -26,3 +26,31 @@ export async function wpQuery({ query, variables = {} }: gqlParams) {
   const { data } = await res.json();
   return data;
 }
+
+// for basic auth
+export async function wpQueryAuth({ query, variables = {} }: gqlParams) {
+  const username = import.meta.env.BASIC_USERNAME;
+  const password = import.meta.env.BASIC_PASSWORD;
+
+  const authString = `Basic ${btoa(`${username}:${password}`)}`;
+
+  const res = await fetch(import.meta.env.WP_URL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": authString,
+    },
+    body: JSON.stringify({
+      query,
+      variables,
+    }),
+  });
+
+  if (!res.ok) {
+    console.error(res);
+    return {};
+  }
+
+  const { data } = await res.json();
+  return data;
+}
