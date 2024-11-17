d583614c1d8db6fefd0c698070420fae8f3b726b
Karl Mantle <130184376+karl-mantle@users.noreply.github.com>
2024-11-17 19:44:04 +0000

    page/post routing refactor

--- src/utils/gqlPosts.ts
+++ src/utils/gqlPosts.ts
@@ -1,10 +1,10 @@
-import { wpQuery } from './wpQuery';
-import type { NodeByUri } from '../types';
+import { wpQuery } from './wpQuery.ts';
+import type { NodeByUri } from '../types.ts';
 
-export async function getRecentPosts(amount: number): Promise<NodeByUri["posts"]> {
+export async function getPostsByMostRecent(amount: number): Promise<NodeByUri["posts"]> {
   const res = await wpQuery({
-    query: `query getRecentPosts($amount: Int!) {
-      posts(first: $amount) {
+    query: `query getPostsByMostRecent($amount: Int!) {
+      posts(first: $amount, where: {orderby: {field: DATE, order: DESC}}) {
         nodes {
           uri
           title
@@ -34,5 +34,79 @@ export async function getRecentPosts(amount: number): Promise<NodeByUri["posts"]
     variables: { amount }
   });
 
-  return res.posts;
+  return res.posts.nodes;
+}
+
+export async function getPostsByTag(tag: string): Promise<NodeByUri["posts"]> {
+  const res = await wpQuery({
+    query: `query getPostsByTag($tag: String) {
+      posts(where: {tag: $tag}) {
+        nodes {
+          uri
+          title
+          excerpt
+          dateGmt
+          categories {
+            nodes {
+              name
+              uri
+            }
+          }
+          featuredImage {
+            node {
+              altText
+              sourceUrl
+            }
+          }
+          tags {
+            nodes {
+              uri
+              name
+            }
+          }
+        }
+      }
+    }`,
+    variables: { tag }
+  });
+
+  const posts = res.posts.nodes;
+  return posts;
+}
+
+export async function getPostsByCategory(category: string): Promise<NodeByUri["posts"]> {
+  const res = await wpQuery({
+    query: `query getPostsByCategory($category: String) {
+      posts(where: {CategoryName: $category}) {
+        nodes {
+          uri
+          title
+          excerpt
+          dateGmt
+          categories {
+            nodes {
+              name
+              uri
+            }
+          }
+          featuredImage {
+            node {
+              altText
+              sourceUrl
+            }
+          }
+          tags {
+            nodes {
+              uri
+              name
+            }
+          }
+        }
+      }
+    }`,
+    variables: { category }
+  });
+
+  const posts = res.posts.nodes;
+  return posts;
 }
