58851997d0004ed844516736a2bf7c7322df3023
Karl Mantle <130184376+karl-mantle@users.noreply.github.com>
2024-11-25 00:32:19 +0000

    favicons + opengraph

---
+++ src/pages/manifest.json.ts
@@ -0,0 +1,43 @@
+import type { APIRoute } from 'astro';
+import type { NodeByUri } from '../types.ts';
+import { getImage } from 'astro:assets';
+import { getNodeByURI } from '../utils/gqlRoutes.ts';
+import faviconSrc from '../../public/favicon.png';
+
+const data = await getNodeByURI("/");
+const node: NodeByUri = data.nodeByUri;
+
+const manifestTitle = node?.seo?.title || 'Error: No title set.';
+const manifestDesc = node?.seo?.metaDesc || 'Error: No meta description set.';
+
+const faviconPngSizes = [192, 256, 512];
+
+export const GET: APIRoute = async () => {
+  const icons = await Promise.all(
+    faviconPngSizes.map(async (size) => {
+      const image = await getImage({
+        src: faviconSrc,
+        width: size,
+        height: size,
+        format: 'png'
+      });
+      return {
+        src: image.src,
+        type: `image/${image.options.format}`,
+        sizes: `${image.options.width}x${image.options.height}`
+      };
+    })
+  );
+
+  const manifest = {
+    name: `${manifestTitle}`,
+    description: `${manifestDesc}`,
+    start_url: '/',
+    display: 'standalone',
+    id: 'some-unique-id',
+    icons,
+  };
+
+  return new Response(JSON.stringify(manifest));
+
+};
---
+++ src/pages/opengraph.jpg.ts
@@ -0,0 +1,19 @@
+import type { APIRoute } from 'astro';
+import sharp from 'sharp';
+import { getOpenGraph } from '../utils/gqlYoastSeo.ts';
+
+const openGraphData = await getOpenGraph();
+const defaultImage = openGraphData.sourceUrl;
+
+export const GET: APIRoute = async () => {
+
+  const response = await fetch(defaultImage);
+  const arrayBuffer = await response.arrayBuffer();
+  const imageBuffer = Buffer.from(arrayBuffer);
+
+  const ogImageBuffer = await sharp(imageBuffer).resize(1200, 630).toFormat('jpeg').toBuffer();
+
+  return new Response(ogImageBuffer, {
+    headers: { 'Content-Type': 'image/jpeg' }
+  });
+};
